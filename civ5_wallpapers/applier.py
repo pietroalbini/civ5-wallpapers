ee40d7fd381b6682443151af7be41c7e58ca8614
Pietro Albini <pietro@pietroalbini.io>
2016-10-04 23:49:17 +0200

    Fix set-random not working under cron
    The cron environment didn't provide the $DBUS_SESSION_BUS_ADDRESS
    environment variable, needed by gsettings.

--- civ5_wallpapers/applier.py
+++ civ5_wallpapers/applier.py
@@ -29,11 +29,32 @@ def get_random_wallpaper(directory):
 
 def set_gnome_wallpaper(wallpaper):
     """Set a wallpaper in the GNOME desktop environment"""
+    env = os.environ.copy()
+
+    # Fix cron support -- it doesn't have access to $DBUS_SESSION_BUS_ADDRESS
+    # http://askubuntu.com/questions/403918/setting-cron-to-run-a-shell-script-random-wallpaper-from-a-webpage
+    if "DBUS_SESSION_BUS_ADDRESS" not in env:
+        # Get the PID of gnome-session
+        pid_raw, _ = subprocess.Popen(
+            ["pgrep", "gnome-session"],
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        ).communicate()
+        pid = pid_raw.decode("ascii").strip()
+
+        # Get $DBUS_SESSION_BUS_ADDRESS from the gnome-session environ
+        with open(os.path.join("/proc", pid, "environ")) as f:
+            session_env = dict(
+                l.split("=", 1) for l in f.read().split("\0") if l.count("=")
+            )
+
+        # Set the correct $DBUS_SESSION_BUS_ADDRESS
+        env["DBUS_SESSION_BUS_ADDRESS"] = session_env["DBUS_SESSION_BUS_ADDRESS"]
+
     subprocess.call([
         "gsettings", "set",
         "org.gnome.desktop.background", "picture-uri",
         "file://%s" % wallpaper
-    ])
+    ], env=env)
 
 
 def set_wallpaper(de, wallpaper):
