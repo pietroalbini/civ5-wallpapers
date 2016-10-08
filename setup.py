9941ab63af6a6804a92ec213dbefc4a58f8538ad
Pietro Albini <pietro@pietroalbini.io>
2016-10-08 18:37:08 +0200

    Update setup.py with more detailed information

--- setup.py
+++ setup.py
@@ -1,5 +1,37 @@
 #!/usr/bin/python3
-# Copyright (c) 2016 Pietro Albini <pietro@pietroalbini.org>
+# Copyright (C) 2016 Pietro Albini <pietro@pietroalbini.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+===============
+civ5-wallpapers
+===============
+
+`Sid Meier's Civilization V`_ is a great turn-based strategy game, and it comes
+with a bunch of great paintings, spread all around the game. This tool allows
+you to extract those, and to use them as your wallpaper for your favourite
+Linux distro.
+
+**You must have a legit copy of Civilization V installed on your computer to
+use this program.**
+
+To learn more about how to use it, check out the `Readme on GitHub`_.
+
+.. _Sid Meier's Civilization V: http://store.steampowered.com/app/8930
+.. _Readme on GitHub: https://github.com/pietroalbini/civ5-wallpapers
+"""
 
 import setuptools
 
@@ -14,6 +46,7 @@ setuptools.setup(
     author_email = "pietro@pietroalbini.org",
 
     description = "Use Civilization V wallpapers on your Linux desktop",
+    long_description = __doc__,
 
     packages = [
         "civ5_wallpapers",
@@ -28,12 +61,16 @@ setuptools.setup(
     zip_safe = False,
 
     classifiers = [
-        "Development Status :: 5 - Production-ready",
-        "Environment :: Other Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: X11 Applications",
+        "Environment :: X11 Applications :: Gnome",
+        "Intended Audience :: Other Audience",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Programming Language :: Python :: 3",
-        "Topic :: Communications :: Chat",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Desktop Environment :: Gnome",
+        "Topic :: Games/Entertainment",
+        "Topic :: Games/Entertainment :: Turn Based Strategy",
+        "Topic :: Utilities",
     ],
 )
